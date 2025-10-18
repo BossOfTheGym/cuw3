6b37956666229ee997a9427d09de8fc04c6a6891
BossOfTheGym <vladimir.deikun@gmail.com>
2025-10-18 17:29:29 +0300

    temp commit: thread_local_allocator

--- cuw3/include/cuw3/thread_local_allocator.hpp
+++ cuw3/include/cuw3/thread_local_allocator.hpp
@@ -2,22 +2,48 @@
 
 #include "conf.hpp"
 #include "list.hpp"
+#include "backoff.hpp"
 #include "retire_reclaim.hpp"
 
-#define CUW3_GRAVEYARD_SLOT_COUNT 8
+#define CUW3_GRAVEYARD_SLOT_COUNT 16
 
 namespace cuw3 {
     inline constexpr usize conf_graveyard_slot_count = CUW3_GRAVEYARD_SLOT_COUNT;
 
-    struct ThreadLocalAllocator;
+    // grave consists of slots and common retire list
+    // first, you attempt to fill the slots, after that (if you failed) you push it to the common list
+    
+    using ThreadGraveyardBackoff = SimpleBackoff;
 
-    struct alignas(conf_cacheline) ThreadLocalAllocatorGraveyardSlot {
-        uint64 count{};
-        void* head{};
+    using ThreadGraveSlotRawPtr = uint64;
+
+    inline constexpr ThreadGraveSlotRawPtr thread_grave_status_bits = 1;
+
+    enum ThreadGraveSlotFlags : ThreadGraveSlotRawPtr {
+        // when set, means that somebody acquired this slot
+        // distributing thread must not put any threads here as it may be occupied back
+        // if slot is not occupied and empty => it is just empty
+        Acquired = 1,
     };
 
-    struct ThreadLocalAllocatorGraveyard {
-        ThreadLocalAllocatorGraveyardSlot slots[conf_graveyard_slot_count] = {};
+    using ThreadGravePtr = AlignmentPackedPtr<ThreadGraveSlotRawPtr, 1>;
+
+
+    struct alignas(conf_cacheline) ThreadGrave {
+        ThreadGravePtr extremely_dead_motherfucker{};
+    };
+
+    struct ThreadGraveObtained {
+        void* extremely_dead_motherfucker{};
+        uint32 grave_num{};
+    };
+
+    struct ThreadGraveyard {
+        ThreadGrave thread_graves[conf_graveyard_slot_count] = {};
+
+        struct alignas(conf_cacheline) {
+            void* dead_body_dump{};
+        };
     };
 
     struct ThreadLocalAllocator {
