0e26d417652f3bf7df4a4bdc20551594b2388493
BossOfTheGym <vladimir.deikun@gmail.com>
2026-01-15 11:27:13 +0300

    intercommit

--- tests/test_atomic.cpp
+++ tests/test_atomic.cpp
@@ -1,10 +1,12 @@
 #include "cuw3/atomic.hpp"
 
+#include <latch>
 #include <vector>
 #include <thread>
 #include <future>
 #include <utility>
 #include <iostream>
+#include <algorithm>
 #include <functional>
 
 using namespace cuw3;
@@ -41,8 +43,7 @@ void test_dispatch() {
         });
     }
     auto results = dispatch(std::move(functions_int));
-    for (auto result :
- results) {
+    for (auto result : results) {
         std::cout << result << " ";
     }
     std::cout << std::endl;
@@ -54,6 +55,98 @@ void test_dispatch() {
         });
     }
     dispatch(std::move(functions_void));
+}
+
+namespace atomic_stack_tests {
+    using StackLinkType = uint64;
+
+    struct StackTraits {
+        using LinkType = StackLinkType;
+
+        static constexpr LinkType null_link = 0xFFFFFFFF;
+        static constexpr LinkType op_failed = 0xFFFFFFFE;
+    };
+
+    struct Stack {
+        StackLinkType top{};
+        StackLinkType limit{};
+    };
+
+    using StackView = AtomicBumpStackView<StackTraits>;
+
+    void check_stack_allocations(const std::vector<StackLinkType>& allocations, const Stack& stack) {
+        CUW3_CHECK(allocations.size() == stack.limit, "invalid count of allocations");
+        for (uint i = 1; i < allocations.size(); i++) {
+            if (allocations[i - 1] >= allocations[i]) {
+                CUW3_ABORT("invalid order of allocated links");
+            }
+        }
+    }
+
+    void test_atomic_stack_st(uint count) {
+        Stack stack{0, count};
+
+        std::vector<StackLinkType> allocated;
+        allocated.reserve(count);
+
+        while (true) {
+            auto view = StackView{&stack.top, stack.limit};
+            auto top = view.bump();
+            if (top == view.null_link) {
+                break;
+            }
+            allocated.push_back(top);
+        }
+        check_stack_allocations(allocated, stack);
+    }
+
+    void test_atomic_stack_mt(uint count, uint threads) {
+        count = (count + threads - 1);
+        count -= count % threads;
+
+        Stack stack{0, count};
+
+        struct ThreadResult {
+            uint thread_id{};
+            std::vector<StackLinkType> allocated{};
+        };
+
+        std::latch latch{threads}; 
+        std::vector<std::function<ThreadResult()>> jobs{};
+        for (uint i = 0; i < threads; i++) {
+            jobs.push_back([i, &latch, &stack] () -> ThreadResult {
+                latch.arrive_and_wait();
+
+                ThreadResult result{i};
+                while (true) {
+                    auto view = StackView{&stack.top, stack.limit};
+                    auto top = view.bump();
+                    if (top == view.null_link) {
+                        break;
+                    }
+                    result.allocated.push_back(top);
+                }
+                return result;
+            });
+        }
+
+        auto thread_results = dispatch(std::move(jobs));
+
+        uint sum = 0;
+        for (auto& result : thread_results) {
+            sum += result.allocated.size();
+        }
+        CUW3_CHECK(sum == stack.limit, "invalid count of allocations");
+
+        std::vector<StackLinkType> allocations{};
+        allocations.reserve(sum);
+        for (auto& result : thread_results) {
+            allocations.insert(allocations.end(), result.allocated.begin(), result.allocated.end());
+        }
+        std::sort(allocations.begin(), allocations.end());
+
+        check_stack_allocations(allocations, stack);
+    }
 }
 
 namespace atomic_list_tests {
@@ -68,7 +161,7 @@ namespace atomic_list_tests {
         using LinkType = ListLinkType;
 
         struct ListHead {
-            LinkType version : 32{}, next : 32{};
+            LinkType version: 32{}, next: 32{};
         };
 
         static constexpr LinkType null_link = 0xFFFFFFFF;
@@ -77,15 +170,19 @@ namespace atomic_list_tests {
 
     struct ListNodeOps {
         void set_next(ListLinkType node, ListLinkType next) {
+            CUW3_CHECK(node < num_nodes, "invalid node id received");
 
+            std::atomic_ref{nodes[node].next}.store(next, std::memory_order_relaxed);
         }
 
         ListLinkType get_next(ListLinkType node) {
+            CUW3_CHECK(node < num_nodes, "invalid node id received");
 
+            return std::atomic_ref{nodes[node].next}.load(std::memory_order_relaxed);
         }
 
         ListDataNode* nodes{};
-        uint64 num_nodes{};
+        ListLinkType num_nodes{};
     };
 
     void test_atomic_list_st() {
@@ -99,5 +196,11 @@ namespace atomic_list_tests {
 
 int main() {
     test_dispatch();
+
+    atomic_stack_tests::test_atomic_stack_st(10000);
+    for (int i = 0; i < 16; i++) {
+        atomic_stack_tests::test_atomic_stack_mt(10000, 8);
+    }
+
     return 0;
 }
